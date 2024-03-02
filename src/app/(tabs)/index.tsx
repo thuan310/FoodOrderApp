de5116a1841104a036714d25281472ba1b3a8118
thuan310 <thuan3102003@gmail.com>
2024-03-03 01:17:41 +0700

    test

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet , Text, View } from 'react-native';
 
 import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
+import Colors from '@/src/constants/Colors';
+
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Text style={styles.title}>Pizza Peperoni</Text>
+      <Text style={styles.price}>$12.99</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginVertical: 10,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+  }
 });
